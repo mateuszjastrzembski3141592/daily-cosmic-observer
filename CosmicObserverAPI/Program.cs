062fbe1e3bf505f8bf41439bff13fbce2035d8ac
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-03-20 15:54:53 +0100

    Add, register, and configure Data Source

--- CosmicObserverAPI/Program.cs
+++ CosmicObserverAPI/Program.cs
@@ -1,14 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using CosmicObserverAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Get the connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string" + "\"DefaultConnection\" not found.");
 
+// Register data context and configure it to SQLite
+builder.Services.AddDbContext<CosmicDbContext>(options => options.UseSqlite(connectionString));
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+/*-----------------------------*/
+
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
