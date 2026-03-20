422644342f587d9a7fb181fd3cdfb4e8a2ad4017
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-03-20 12:53:38 +0100

    Add core domain models for Events, Logs, and Tags

---
+++ CosmicObserverAPI/Models/CosmicEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace CosmicObserverAPI.Models;
+
+public class CosmicEvent
+{
+    public int Id { get; init; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required DateTime Date { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string SourceUrl { get; set; }
+}
---
+++ CosmicObserverAPI/Models/CosmicTag.cs
@@ -0,0 +1,8 @@
+﻿namespace CosmicObserverAPI.Models;
+
+public class CosmicTag
+{
+    public int Id { get; init; }
+    public required string Name { get; set; }
+    public List<CosmicLog> Logs { get; set; } = []; // Many-to-Many relation with CosmicLog
+}
