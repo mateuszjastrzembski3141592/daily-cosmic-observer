edd536588d60b9fc91cc73b7ea418b7f899d4392
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-06-17 21:27:09 +0200

    WIP: implement ToSanitizedString extensions method

--- CosmicObserverAPI/Extensions/StringSanitizationExtensions.cs
+++ CosmicObserverAPI/Extensions/StringSanitizationExtensions.cs
@@ -2,4 +2,16 @@
 
 public static class StringSanitizationExtensions
 {
+    public static string ToSanitizedString(this string input)
+    {
+        string result = input.Trim().ToLowerInvariant();
+
+        var output = string.Concat(result.Where(c => char.IsAsciiLetterOrDigit(c) || c == ' '));
+
+        var subStrings = output.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        output = String.Join('-', subStrings);
+
+        return output;
+    }
 }
