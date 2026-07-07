09a5d8875fe5025b3122533d9d9d0b07f7c7dccf
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-07-07 15:30:24 +0200

    WIP: implement initial result and error classes

---
+++ CosmicObserverAPI/Shared/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace CosmicObserverAPI.Shared;
+
+public class Error
+{
+    public string Message { get; }
+    public string Code { get; }
+
+    public Error(string message, string code)
+    {
+        Message = message;
+        Code = code;
+    }
+}
---
+++ CosmicObserverAPI/Shared/Result.cs
@@ -0,0 +1,47 @@
+﻿namespace CosmicObserverAPI.Shared;
+
+public class Result<T>
+{
+    public T? Value { get; }
+    public Error? Error { get; }
+    public bool IsSuccess => Error == null;
+
+    private Result(T value)
+    {
+        Value = value;
+        Error = null;
+    }
+
+    private Result(Error error)
+    {
+        Value = default;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Failure(Error error) => new(error);
+
+    public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<Error, TResult> onFailure)
+    {
+        return IsSuccess ? onSuccess(Value!) : onFailure(Error!);
+    }
+}
+
+public class Result
+{
+    public Error? Error { get; }
+    public bool IsSuccess => Error == null;
+
+    private Result(Error? error)
+    {
+        Error = error;
+    }
+
+    public static Result Success() => new(null);
+    public static Result Failure(Error error) => new(error);
+
+    public TResult Map<TResult>(Func<TResult> onSuccess, Func<Error, TResult> onFailure)
+    {
+        return IsSuccess ? onSuccess() : onFailure(Error!);
+    }
+}
